fa32d2dfa1d112b7ab4eddc43523d6a590f9b0a7
Grace Zhang <t-grzhan@microsoft.com>
2020-04-02 13:50:58 -0700

    use published npm pkgs

--- private-templates-service/startup.sh
+++ private-templates-service/startup.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 
-cd adaptivecards-templating-service
+cd client
 npm install
-cd ../adaptivecards-templating-client/typescript
-npm install
-npm run build
-npm link
-cd ../../client
-rm -rf node_modules/ package-lock.json
-npm install
-npm link adaptive-templating-service-typescript-node
-cd src
-npm link adaptive-templating-service-typescript-node
-cd ../../adaptivecards-templating-service
-npm link
 cd ../server
-npm link adaptivecards-templating-service
 npm install
 npm run deploy
 npm run dev 
